7470bad086ac141d6fc078c130c5040f3bf1c6eb
Ben <ben@ben.geek.nz>
2013-05-27 22:04:35 +1200

    Setting XHR transport

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express();
 var server = app.listen(process.env.PORT ? process.env.PORT : 1337);
 var io = require('socket.io').listen(server);
+io.set('transports', ['xhr-polling']);
 
 var config = require('./config');
 
